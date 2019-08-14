fbec7f525f606fe9c331fc53ae4b0c1078d06fe7
Nosau Aliaksandr <plcgi1@gmail.com>
2019-08-14 18:08:02 +0300

    node ts server for sa-web

--- sa-nodejs-webapp/src/helpers/validate-env.ts
+++ sa-nodejs-webapp/src/helpers/validate-env.ts
@@ -4,11 +4,7 @@ import {
 
 export default function validateEnv() {
     cleanEnv(process.env, {
-        JWT_SECRET: str(),
-        MONGO_HOST: str(),
-        MONGO_PASSWORD: str(),
-        MONGO_PORT: port(),
-        MONGO_USER: str(),
-        PORT: port(),
+        SA_LOGIC_API_URL: str(),
+        PORT: str()
     });
 }
